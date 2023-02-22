a1a4ea2072c3f68a504029d49ff31443b148f438
Wesley Prado <wesleypradobjj@gmail.com>
2023-02-22 18:18:41 +0400

    Creating the project

---
+++ appleFrameworksAPP/appleFrameworksAPP/appleFrameworksAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  appleFrameworksAPPApp.swift
+//  appleFrameworksAPP
+//
+//  Created by Wesley Prado on 22/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct appleFrameworksAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FrameworkGridView()
+        }
+    }
+}
